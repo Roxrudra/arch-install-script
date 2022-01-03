2b1f6d4862fe810195ebaaab9d00877b7678cc9b
Rudra <75825041+Roxrudra@users.noreply.github.com>
2022-01-03 17:17:35 +0530

    Add files via upload

--- script_install_2.sh
+++ script_install_2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Set timezone
-timedatectl set-timezone $1
-timedatectl set-ntp true
+/bin/timedatectl set-timezone $1
+/bin/timedatectl set-ntp true
 
 # Generate locale
-sed -i 's/# '$2'/'$2'/' som.txt
-locale-gen
+sed -i 's/#'$2'/'$2'/' /etc/locale.gen
+/bin/locale-gen
 echo "LANG=$2"  > /etc/locale.conf
 export LANG=$2
 
@@ -15,12 +15,12 @@ echo "$3" > /etc/hostname
 echo -e "127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t$3.localdomain\t$3"
 
 # Set root password
-echo "$6" | passwd
+echo "$6" | /bin/passwd
 
 # Add sudo user
 useradd --create-home $4
-echo $5 | passwd $4
-usermod -a -G wheel $4
+echo $5 | /bin/passwd $4
+usermod -aG wheel $4
 vim /etc/sudoers -c ':%s/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/|:wq!'
 
 # Install git and efibootmgr and bootloader
@@ -46,7 +46,7 @@ systemctl enable NetworkManager.service
 ################################################################################
 
 
-echo "sudo pacman -S --noconfirm xdg-user-dirs gvfs-mtp mesa mesa-demos mesa-utils xf86-video-amdgpu amdvlk xf86-input-evdev xf86-input-synaptics xf86-input-wacom xf86-libinput xorg xorg-xinit i3-gaps rofi kitty python python3 xss-lock nitrogen neofetch alsa-utils pavucontrol pulseaudio pulseaudio-bluetooth bluez bluez-utils blueman wget ttf-font-awesome ttf-nerd-fonts-symbols ttf-droid ttf-liberation firefox vlc p7zip evince gtk-chtheme lxappearence imagemagick cheese" > /home/$4/my
+echo "sudo pacman -S --noconfirm xdg-user-dirs gvfs-mtp mesa mesa-demos mesa-utils xf86-video-amdgpu amdvlk xf86-input-evdev xf86-input-synaptics xf86-input-wacom xf86-input-libinput xorg xorg-xinit i3-gaps rofi kitty python python3 xss-lock nitrogen neofetch alsa-utils pavucontrol pulseaudio pulseaudio-bluetooth bluez bluez-utils blueman wget ttf-font-awesome ttf-nerd-fonts-symbols ttf-droid ttf-liberation firefox vlc p7zip evince gtk-chtheme lxappearance imagemagick cheese gtk2 gtk3 gtk4 adapta-gtk-theme" > /home/$4/my
 
 echo "yay -S polybar picom pipes.sh i3lock-color betterlockscreen siji-git" >> /home/$4/my
 
