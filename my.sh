8bdabdc70a63ef0dea6ec3f430073cb2c43eae1d
Rudra <75825041+Roxrudra@users.noreply.github.com>
2022-01-06 10:48:58 +0530

    Update my.sh

--- my.sh
+++ my.sh
@@ -8,6 +8,7 @@ sudo pacman -S --noconfirm alsa-utils pavucontrol pulseaudio pulseaudio-bluetoot
 sudo pacman -S --noconfirm wget 
 sudo pacman -S --noconfirm ttf-font-awesome ttf-nerd-fonts-symbols ttf-droid ttf-liberation
 sudo pacman -S --noconfirm vlc p7zip evince firefox gtk-chtheme lxappearance imagemagick cheese gtk2 gtk3 gtk4 adapta-gtk-theme ranger
+sudo pacman -S --noconfirm zathura zathura-pdf-mupdf fd fzf
 sudo pacman -S --noconfirm cmus htop
 
 yay -S polybar picom-ibhagwan-git pipes.sh i3lock-color betterlockscreen siji-git cava selectdefaultapplication-git
